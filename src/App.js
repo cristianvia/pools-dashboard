d6278e8a3d8d6cd48fd0824df86e831ba30a6d94
cristianvia <cristianviar@gmail.com>
2023-05-03 10:25:18 +0200

    Metamask login btn working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import MetamaskButton from './MetamaskButton';
+import { useState } from 'react';
 
 function App() {
+  const [walletAddress, setWalletAddress] = useState(null);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <MetamaskButton onConnect={setWalletAddress} />
+          {walletAddress && <p> {walletAddress}</p>}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/MetamaskButton.js
@@ -0,0 +1,43 @@
+import './MetamaskBtn.css';
+
+import { useState } from 'react';
+import detectEthereumProvider from '@metamask/detect-provider';
+import Web3 from 'web3';
+
+const MetamaskButton = ({ onConnect }) => {
+  const [address, setAddress] = useState(null);
+
+  const connectToMetamask = async () => {
+    try {
+      const provider = await detectEthereumProvider();
+      if (provider) {
+        const web3 = new Web3(provider);
+        const accounts = await web3.eth.requestAccounts();
+        const address = accounts[0];
+        setAddress(address);
+        onConnect(address);
+      } else {
+        console.error('Metamask not installed');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (address) {
+    return (
+      <p>
+        ¡Bienvenido!
+      </p>
+    );
+  } else {
+    return (
+      <button onClick={connectToMetamask}>
+        <img src="https://raw.githubusercontent.com/MetaMask/brand-resources/master/SVG/metamask-fox.svg" alt="Metamask icon" />
+        Conectar con Metamask
+      </button>
+    );
+  }
+};
+
+export default MetamaskButton;
